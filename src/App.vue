5b0f7c14d44267bdb768a1c7fd30502bc766f80e
Magnun A V F <magnunavf@gmail.com>
2021-02-17 03:37:08 -0300

    Create panel for each photo (improve layout)

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
+  <div class="body">
+    <h1 class="title">{{ title }}</h1>
 
-    <ul>
-      <li v-for="photo of photos">
-        <img :src="photo.url" :alt="photo.title" />
+    <ul class="photos-list">
+      <li class="photos-list-item" v-for="photo of photos">
+        <div class="panel">
+          <h2 class="panel-title">{{ photo.title }}</h2>
+          <div class="panel-content">
+            <img class="responsive-image" :src="photo.url" :alt="photo.title" />
+          </div>
+        </div>
       </li>
     </ul>
   </div>
@@ -20,14 +25,58 @@ export default {
   },
 
   created() {
-    this.$http.get('http://localhost:3000/v1/photos')
+    this.$http
+      .get("http://localhost:3000/v1/photos")
       .then(res => res.json())
       .then(
-        photos => this.photos = photos,
+        photos => (this.photos = photos),
         err => console.log(err)
       );
   }
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.title {
+  text-align: center;
+}
+
+.body {
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+}
+
+.photos-list {
+  list-style: none;
+}
+
+.photos-list .photos-list-item {
+  display: inline-block;
+}
+
+.responsive-image {
+  width: 100%;
+}
+
+.panel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.panel .panel-title {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
+</style>
