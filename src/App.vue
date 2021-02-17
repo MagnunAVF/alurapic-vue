57cc99cf441935b37b9e7a5e55598e34a6a73b2d
Magnun A V F <magnunavf@gmail.com>
2021-02-17 02:34:33 -0300

    Use v-for to show multiple images

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <img :src="photo.url" :alt="photo.title">
+
+    <ul>
+      <li v-for="photo of photos">
+        <img :src="photo.url" :alt="photo.title" />
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -9,11 +14,19 @@
 export default {
   data() {
     return {
-      title: 'Alurapic',
-      photo: {
-        url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
-        title: 'Dog',
-      }
+      title: "Alurapic",
+      photos: [
+        {
+          url:
+            "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It",
+          title: "Dog"
+        },
+        {
+          url:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOhmlmzV4-Sifx5BIc2SXeA-1CtZJf8jb8V_vPZyKbXIQJKU-rkxGO6OM",
+          title: "Cat"
+        }
+      ]
     };
   }
 };
