1559a7a652393c434e18201516c45fc359bb6bef
Magnun A V F <magnunavf@gmail.com>
2021-02-18 05:27:20 -0300

    Create Home and Register page

---
+++ src/components/home/Home.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <h1 class="title">{{ title }}</h1>
+
+    <input
+      type="search"
+      class="filter"
+      @input="filter = $event.target.value"
+      placeholder="Filter by photo title"
+    />
+
+    <ul class="photos-list">
+      <li class="photos-list-item" v-for="photo of photosWithFilter">
+        <panel :title="photo.title">
+          <responsive-image
+            :url="photo.url"
+            :title="photo.title"
+          ></responsive-image>
+        </panel>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Panel from "../shared/panel/Panel.vue";
+import ResponsiveImage from "../shared/responsive-image/ResponsiveImage.vue";
+
+export default {
+  components: {
+    panel: Panel,
+    "responsive-image": ResponsiveImage
+  },
+
+  data() {
+    return {
+      title: "Alurapic",
+      photos: [],
+      filter: ""
+    };
+  },
+
+  computed: {
+    photosWithFilter() {
+      if (this.filter) {
+        let regex = new RegExp(this.filter.trim(), "i");
+        return this.photos.filter(photo => regex.test(photo.title));
+      } else {
+        return this.photos;
+      }
+    }
+  },
+
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/photos")
+      .then(res => res.json())
+      .then(
+        photos => (this.photos = photos),
+        err => console.log(err)
+      );
+  }
+};
+</script>
+
+<style lang="scss">
+.title {
+  text-align: center;
+}
+
+.photos-list {
+  list-style: none;
+}
+
+.photos-list .photos-list-item {
+  display: inline-block;
+}
+
+.filter {
+  display: block;
+  width: 100%;
+}
+</style>
---
+++ src/components/register/Register.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2 class="title">Register</h2>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.title {
+  text-align: center;
+}
+</style>
