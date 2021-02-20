88a0b9bcbb240e7c36a6838f11f384bc7a1b6d96
Magnun A V F <magnunavf@gmail.com>
2021-02-20 04:28:33 -0300

    Create Register Form initial structure

--- src/components/register/Register.vue
+++ src/components/register/Register.vue
@@ -1,14 +1,67 @@
 <template>
   <div>
-    <h1 class="title">Register</h1>
+    <h1 class="centered">Photo Register</h1>
+    <h2 class="centered"></h2>
+
+    <form>
+      <div class="control">
+        <label for="title">Title</label>
+        <input id="title" autocomplete="off" />
+      </div>
+
+      <div class="control">
+        <label for="url">URL</label>
+        <input id="url" autocomplete="off" />
+        <responsive-image title="" url=""/>
+      </div>
+
+      <div class="control">
+        <label for="description">Description</label>
+        <textarea id="description" autocomplete="off"></textarea>
+      </div>
+
+      <div class="centered">
+        <custom-button label="Save" type="submit"/>
+        <router-link to="/"
+          ><custom-button label="return" type="button"
+        /></router-link>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-</script>
+import ResponsiveImage from "../shared/responsive-image/ResponsiveImage.vue";
+import Button from "../shared/button/Button.vue";
 
-<style>
-.title {
+export default {
+  components: {
+    "responsive-image": ResponsiveImage,
+    "custom-button": Button
+  }
+};
+</script>
+<style scoped>
+.centered {
+  text-align: center;
+}
+.control {
+  font-size: 1.2em;
+  margin-bottom: 20px;
+}
+.control label {
+  display: block;
+  font-weight: bold;
+}
+
+.control label + input,
+.control textarea {
+  width: 100%;
+  font-size: inherit;
+  border-radius: 5px;
+}
+
+.centered {
   text-align: center;
 }
 </style>
